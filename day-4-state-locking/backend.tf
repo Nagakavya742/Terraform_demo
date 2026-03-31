29413fd3fefa95352bfd398288cfa88b0d121070
Naga Kavya <nagakavyakakumanu@gmail.com>
2026-03-31 18:51:08 +0530

    working with dyanamoDB

--- day-4-state-locking/backend.tf
+++ day-4-state-locking/backend.tf
@@ -2,8 +2,10 @@ terraform {
   backend "s3" {
     bucket = "kavya-unique-bucket"     
     key = "terraform.tfstate"     #statefile is the key to apply in s3 bucket 
-    use_lockfile = true         #use s3 native locking
+    use_lockfile = true         #use s3 native locking  locking and useful for above 1.19 version only
     region = "us-east-1"
-    
+    dynamodb_table = "kavya"    #any version we can use dynamodb   dynamodb name should be statefile name only
+    encrypt = true    #both can't be used ate same time either state lock or dynamodb
+            
   }
 }
