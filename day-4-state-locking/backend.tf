3e78e3f1a893d55fdb3d709ba632d7d97b52899a
Naga Kavya <nagakavyakakumanu@gmail.com>
2026-03-31 18:18:53 +0530

    lock added to statue file

--- day-4-state-locking/backend.tf
+++ day-4-state-locking/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket = "kavya-unique-bucket"     
     key = "terraform.tfstate"     #statefile is the key to apply in s3 bucket 
+    use_lockfile = true         #use s3 native locking
     region = "us-east-1"
     
   }
