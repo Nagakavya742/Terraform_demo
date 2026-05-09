058affaa82e30ba3efdda067254f50706cd14767
Naga Kavya <nagakavyakakumanu@gmail.com>
2026-05-09 21:40:41 +0530

    Added Terraform project properly

--- day-2-terraform-configurations-files/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "kavya-unique-bucket"     
-    key = "day-2/terraform.tfstate"     #automatically create folder and don't override the existing statefile code  
-    region = "us-east-1"
-    
-  }
-}
--- day-2-terraform-configurations-files/provider.tf
+++
@@ -1,3 +0,0 @@
-provider "aws" {
-  region = "us-east-1"   #explicit mentioning by default it take from ur aws config token keys
-}
