8b22fa779b42427038549bca3d4e323cf0cec575
Naga Kavya <nagakavyakakumanu@gmail.com>
2026-05-11 23:34:53 +0530

    working with lifecycle and target group

---
+++ day-5-lifecycles-rules/main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "name" {
+  ami = "ami-0a59ec92177ec3fad"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "test"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+  lifecycle {
+    ignore_changes = [ tags, ]
+  }
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
+}
---
+++ day-5-target-resources/main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "name" {
+  ami = "ami-0a59ec92177ec3fad"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "test"
+  }
+  
+}
+resource "aws_s3_bucket" "name" {
+    bucket = "kavya-unique-bucket"
+  }
+
+#in this 2 resources will create if we apply but we need only one
+#so we can restrict her by giving command
+#terraform plan -target=aws_s3_bucket.name
+#target resource we use to apply
