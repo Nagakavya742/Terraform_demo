1b1eb893a5130cce686faaf7a593b6e486c9117b
Naga Kavya <nagakavyakakumanu@gmail.com>
2026-06-09 10:28:57 +0530

    working with multiple workspace in same main.tf for creating same resource in diff workspace

---
+++ day-11-workspace/main.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "name"{
+  bucket = "kavya-unique-bucketttt"
+}
+
+# terraform workspace 
+
+#  terraform workspace
+# Usage: terraform [global options] workspace
+
+#   new, list, show, select and delete Terraform workspaces.
+
+# Subcommands:
+#     delete    Delete a workspace
+#     list      List Workspaces
+#     new       Create a new workspace
+#     select    Select a workspace
+#     show      Show the name of the current workspace
