4a40deaa1d2f0e0dd101d23102a07440d0f43be0
Naga Kavya <nagakavyakakumanu@gmail.com>
2026-06-10 23:41:31 +0530

    creating multiple sg groups with same cidr

---
+++ day-12-multiple-sg-rules-sameCIDR/provider.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "project"{
+  name ="devops-project"
+  description =  "Allow TLS inbound traffic"
+  
+  ingress = {
+    for port in [22, 80, 443, 8080, 9000, 3000, 8082, 8081] : port => {
+      description = "inbound rules"
+      from_port = port
+      to_port = port
+      protocol = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+      ipv6_cidr_blocks= []
+      prefix_list_ids = []
+      security_groups = []
+      self = false
+    }
+  }
+
+  egress {
+    from_port = 0
+    to_port= 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+}
+
+#for each is used for creating multiple resources
+#all ports are same with  cidr same 
